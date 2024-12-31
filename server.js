3e394f983ef2302e4070aecf99230a0bdc2e42ab
Jonas-Petrikas <jpetrik15@students.lcc.lt>
2024-12-31 16:38:07 +0200

    Back-office added

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const bodyParser = require('body-parser');
 const handlebars = require('handlebars');
+const URL = 'http://localhost:3000/'
 
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const makeHtml = (data, page, back = true) => {
     const dir = back ? 'back' : 'front';
-    data.url = 'http://localhost:3000/';
+    data.url = URL;
     const topHtml = fs.readFileSync(`./templates/${dir}/top.hbr`, 'utf8');
     const bottomHtml = fs.readFileSync(`./templates/${dir}/bottom.hbr`, 'utf8');
     const pageHtml = fs.readFileSync(`./templates/${dir}/${page}.hbr`, 'utf8');
@@ -17,9 +20,6 @@ const makeHtml = (data, page, back = true) => {
 }
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 app.get('/admin', (req, res) => {
 
@@ -33,6 +33,62 @@ app.get('/admin', (req, res) => {
 
     res.send(html);
 });
+
+app.get('/admin/page-top', (req, res) => {
+
+    let mainTopData = fs.readFileSync('./data/main-top.json', 'utf8');
+    mainTopData = JSON.parse(mainTopData);
+
+
+    const data = {
+        pageTitle: 'Pagrindinio puslapio viršus',
+        mainTopData,
+    };
+
+    const html = makeHtml(data, 'pageTop');
+
+
+    res.send(html);
+});
+
+app.post('/admin/page-top', (req, res) => {
+
+    const { main_title, sub_title, page_text } = req.body;
+    // will be validated later
+
+    let mainTopData = {
+        main_title,
+        sub_title,
+        page_text,
+    };
+
+    mainTopData = JSON.stringify(mainTopData);
+
+    fs.writeFileSync('./data/main-top.json', mainTopData);
+
+    res.redirect(URL + 'admin/page-top');
+
+});
+
+app.get('/', (req, res) => {
+    let mainTopData = fs.readFileSync('./data/main-top.json', 'utf8');
+    mainTopData = JSON.parse(mainTopData);
+
+    const data = {
+        pageTitle: 'First page',
+        mainTopData,
+    };
+
+    mainTopData.page_text = mainTopData.page_text.split('\n');
+
+
+    const html = makeHtml(data, 'landing', false);
+
+
+    res.send(html);
+});
+
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Pasiruoses ir laukia ${port}`);
